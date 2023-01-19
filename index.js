f4e248fb806444d8cf5a93e398d10dfad3cb019a
Gerson Aranibar <aranibargerson28@gmail.com>
2023-01-18 21:23:48 -0500

    update files

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(compression());
-mongoose.set('strictQuery', true);
+
 mongoose.connect(process.env.MONGO_DB).then(() => console.log("DB Online"));
 
 
